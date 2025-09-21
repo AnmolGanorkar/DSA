a84623e01d8421dcdd3227e5c4f448c30b69c709
ANMOL GANORKAR <anmolganorkar@gmail.com>
2025-09-21 19:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (36.13%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
-    int fib(int n) {
-        
-        if(n==0) return 0;
-        if(n==1) return 1;
-
-        int ans = fib(n - 1) + fib(n - 2);
-        
-        return ans;
+int fib(int n) {
+    int fn=0;
+    int f0=0;
+    int f1=1;
+    if(n<=1)
+    {
+        return n;
+    }
+        for(int i=1;i<n;i++)
+        {
+            fn=f0+f1;
+            f0=f1;
+            f1=fn;
+        }
+        return fn;
     }
 };
