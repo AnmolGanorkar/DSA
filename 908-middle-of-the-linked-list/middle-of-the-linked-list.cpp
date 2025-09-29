3e600d556854bb32c66020c7f4448aca921fd020
ANMOL GANORKAR <anmolganorkar@gmail.com>
2025-09-29 23:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (88.93%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,27 +10,29 @@
  */
 class Solution {
 public:
+    // fast and slow pointer appoarch
+    ListNode* getMiddle(ListNode* head){
 
-    int getLength(ListNode* head){
-        int len = 0;
-        while(head!=NULL){
-            len++;
-            head=head->next;
+        if(head ==NULL|| head->next == NULL) return head;
+
+        // 2 nodes only
+        if(head->next->next==NULL) return head->next;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast!=NULL){
+            fast = fast->next;
+            if(fast!=NULL){
+                fast = fast->next;
+            }
+
+            slow = slow-> next;
         }
-
-        return len;
+        return slow;
     }
+
     ListNode* middleNode(ListNode* head) {
-        int len = getLength(head);
-        int ans =(len/2);
-
-        ListNode* temp =head;
-        int cnt=0;
-        while(cnt<ans){
-            temp=temp->next;
-            cnt++;
-        }
-
-        return temp;
+        return getMiddle(head);
     }
 };
