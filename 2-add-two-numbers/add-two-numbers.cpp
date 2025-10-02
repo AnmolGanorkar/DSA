d3f860a443f4b317a3c07008f257189278302665
ANMOL GANORKAR <anmolganorkar@gmail.com>
2025-10-03 02:33:19 +0530

    Time: 3 ms (34.06%) | Memory: 77.3 MB (14.06%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -8,91 +8,56 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 private:
-    ListNode* reverse(ListNode* head){
-            ListNode* curr=head;
-            ListNode* prev = NULL;
-            ListNode* next =NULL;
-
-            while(curr!=NULL){
-
-            next=curr->next;
-            curr->next = prev;
-            prev=curr;
-            curr=next;
-            }
-            return prev;
+    void insertAtTail(ListNode* &head, ListNode* &tail, int val){
+        ListNode* temp = new ListNode(val);
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+        } else {
+            tail->next = temp;
+            tail = temp;
         }
-        void insertAtTail(ListNode* &head, ListNode* &tail, int val){
-            ListNode* temp = new ListNode(val);
-            //empty list
-            if(head==NULL){
-                head=temp;
-                tail=temp;
-                return;
+    }
+
+    ListNode* add(ListNode* l1, ListNode* l2){
+        int carry = 0;
+        ListNode* anshead = NULL;
+        ListNode* anstail = NULL;
+
+        while(l1 != NULL || l2 != NULL || carry != 0){
+            int sum = carry;
+            if(l1 != NULL){
+                sum += l1->val;
+                l1 = l1->next;
             }
-            else{
-                tail->next=temp;
-                tail=temp;
+            if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
             }
+
+            int digit = sum % 10;
+            insertAtTail(anshead, anstail, digit);
+            carry = sum / 10;
         }
 
-        ListNode* add(ListNode* l1, ListNode* l2){
-            int carry=0;
-            ListNode* anshead=NULL;
-            ListNode* anstail=NULL;
-
-            while(l1!=NULL && l2!=NULL){
-                int sum = carry + l1->val + l2->val;
-
-                int digit = sum%10;
-                // create node and add answer in L.L
-                insertAtTail(anshead,anstail,digit);
-
-                carry = sum/10;
-                l1=l1->next;
-                l2=l2->next;
-            }
-
-            while(l1!=NULL){
-                int sum = carry +l1->val;
-                 int digit = sum%10;
-                // create node and add answer in L.L
-                insertAtTail(anshead,anstail,digit);
-                carry = sum/10;
-                l1=l1->next;
-            }
-            while(l2!=NULL){
-                int sum = carry +l2->val;
-                 int digit = sum%10;
-                // create node and add answer in L.L
-                insertAtTail(anshead,anstail,digit);
-                carry = sum/10;
-                l2=l2->next;
-            }
-            while(carry!=0){
-                int sum = carry;
-                 int digit = sum%10;
-                // create node and add answer in L.L
-                insertAtTail(anshead,anstail,digit);
-                carry = sum/10;
-            }
-            return anshead;
-        }
+        return anshead;
+    }
 
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        //step 1: reverse l1 list
-       // l1=reverse(l1);
-        //step 2: reverse l2 list
-        //l2=reverse(l2);
-
-        //step 3: add 2 L.L values
-        ListNode* ans = add(l1,l2);
-        //ans = reverse(ans);
-
-        return ans;
-
+        // Directly add the numbers without reversing
+        return add(l1, l2);
     }
 };
