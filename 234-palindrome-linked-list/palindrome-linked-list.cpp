23f31e8668ecaf88c8674d6ff3673620b34109ae
ANMOL GANORKAR <anmolganorkar@gmail.com>
2025-10-03 01:23:45 +0530

    Time: 2 ms (66.58%) | Memory: 121.8 MB (54.19%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,33 +9,64 @@
  * };
  */
 class Solution {
-private: 
-    bool solve(vector<int> arr){
-        int n = arr.size();
-        int start = 0;
-        int end = n-1;
+    private:
+        ListNode* getMid(ListNode* head){
+            // to get middle we gonna use fast and slow pointers
 
-        while(start<end){
-            if(arr[start]!=arr[end]){
-                return false;
+            ListNode* slow = head;
+            ListNode* fast = head-> next;
+
+            while(fast!=NULL && fast->next!=NULL){
+                fast=fast->next->next;
+                slow=slow->next;
             }
-            start++;
-            end--;
+            return slow;
         }
 
-        return true;
-    }
+        ListNode* reverse(ListNode* head){
+            ListNode* curr=head;
+            ListNode* prev = NULL;
+            ListNode* next =NULL;
+
+            while(curr!=NULL){
+
+            next=curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr=next;
+            }
+            return prev;
+        }
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
+        if (head==NULL || head->next == NULL){
+            return true;
+        }
+        // step 1: find middle
+        ListNode* middle = getMid(head);
 
-        while(temp!=NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
+        //step 2: reverse list after middle
+        ListNode* temp = middle->next;
+
+        middle->next=reverse(temp);
+
+        //step 3: compare both half's
+        ListNode* head1 = head;
+        ListNode* head2= middle->next;
+
+        while(head2!=NULL){
+            if(head1->val != head2->val){
+                return false;
+            }
+
+            head1=head1->next;
+            head2= head2->next;
         }
 
-        return solve(arr);
-    
+        //step 4: repeat step 2
+        temp = middle->next;
+        middle->next=reverse(temp);
+        return true;
+
     }
 };
